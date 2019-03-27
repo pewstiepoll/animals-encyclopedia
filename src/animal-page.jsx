5a0509ac83a6f946693522c9650665a0bb19c535
Vadym <wadedevelop@gmail.com>
2019-03-27 11:21:39 +0200

    timeout for better animations. FormatOnSave

--- src/animal-page.jsx
+++ src/animal-page.jsx
@@ -3,93 +3,101 @@ import ImageSlider from "./image-slider";
 import { makeImgUrl } from "./helpers";
 import styles from "./animal-page.module.css";
 
-function AnimalDescription ({scientificName, type}) {
-    return (
-        <div className={styles.description}>
-            <span>
-                Scientific name: {scientificName}
-            </span>
-            <span>
-                Type: {type}
-            </span>
-        </div>
-    )
+function AnimalDescription({ scientificName, type }) {
+  return (
+    <div className={styles.description}>
+      <span>Scientific name: {scientificName}</span>
+      <span>Type: {type}</span>
+    </div>
+  );
 }
 
-function Header ({ animal }) {
-    const { type, scientific_name: scientificName } = animal;
+function Header({ animal }) {
+  const { type, scientific_name: scientificName } = animal;
 
-    return (
-        <div>
-            <h1 className={styles.title}>Wild Animals</h1>
-            <AnimalDescription type={type} scientificName={scientificName} />
-        </div>
-    )
+  return (
+    <div>
+      <h1 className={styles.title}>Wild Animals</h1>
+      <AnimalDescription type={type} scientificName={scientificName} />
+    </div>
+  );
 }
 
 export default class AnimalPage extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            isLoading: true,
-            imageId: 0,
-            image: null
-        }
-    }
+    this.state = {
+      isLoading: true,
+      imageId: 0,
+      image: null
+    };
+  }
 
-    _handleImageClick = (id) => {
-        const { animal } = this.props;
-        const { imageId : prevImageId, image: prevImage, isLoading } = this.state;
+  _handleImageClick = id => {
+    const { animal } = this.props;
+    const { imageId: prevImageId, image: prevImage, isLoading } = this.state;
 
-        if (id === prevImageId || isLoading) return;
+    if (id === prevImageId || isLoading) return;
 
-        this.setState({ isLoading: true });
+    this.setState({ isLoading: true });
 
-        const image = new Image();
-        image.src = makeImgUrl(animal.images[id].url) || prevImageId;
+    const image = new Image();
+    image.src = makeImgUrl(animal.images[id].url) || prevImageId;
 
-        image.onload = () => {
-            this.setState({ isLoading: false, image: image.src, imageId: id });
-        }
+    image.onload = () => {
+      setTimeout(() => {
+        this.setState({ isLoading: false, image: image.src, imageId: id });
+      }, 2000);
+    };
 
-        image.onerror = () => {
-            this.setState({ isLoading: false, image: prevImage });
-        }
-    }
+    image.onerror = () => {
+      this.setState({ isLoading: false, image: prevImage });
+    };
+  };
 
-    componentWillMount() {
-        const { animal } = this.props;
-        const { imageId } = this.state;
+  componentWillMount() {
+    const { animal } = this.props;
+    const { imageId } = this.state;
 
-        const defaultImageUrl = animal.images[imageId].url;
+    const defaultImageUrl = animal.images[imageId].url;
 
-        const image = new Image();
+    const image = new Image();
 
-        image.src = makeImgUrl(defaultImageUrl);
+    image.src = makeImgUrl(defaultImageUrl);
 
-        image.onload = () => {
-            this.setState({
-                isLoading: false,
-                image: image.src
-            });
-        };
-    }
+    image.onload = () => {
+      this.setState({
+        isLoading: false,
+        image: image.src
+      });
+    };
+  }
 
-    render() {
-        const { animal } = this.props;
-        const { image, imageId, isLoading } = this.state;
+  render() {
+    const { animal } = this.props;
+    const { image, imageId, isLoading } = this.state;
 
-        const classes = [styles.backgroundImage, isLoading ? styles.backgroundImageLoading : styles.backgroundImageLoaded].join(" ");
+    const classes = [
+      styles.backgroundImage,
+      isLoading ? styles.backgroundImageLoading : styles.backgroundImageLoaded
+    ].join(" ");
 
-        return image && (
-            <div className={styles.container}>
-                <div className={styles.wrapper}>
-                    <Header animal={animal} />
-                    <ImageSlider current={imageId + 1} onImageClick={this._handleImageClick} name={animal.name} images={animal.images} />
-                </div>
-                <img className={classes} src={this.state.image} alt="background"/>
-            </div>
-        )
-    }
+    return (
+      image && (
+        <div className={styles.container}>
+          <div className={styles.wrapper}>
+            <Header animal={animal} />
+            <ImageSlider
+              current={imageId + 1}
+              onImageClick={this._handleImageClick}
+              name={animal.name}
+              images={animal.images}
+            />
+          </div>
+          <img className={classes} src={this.state.image} alt="background" />
+        </div>
+      )
+    );
+  }
 }
